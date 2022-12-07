d581d35d1f8f0d198b6b21204e151774e495280a
alehandru <alexandru.lemnaru@solarwinds.com>
2022-12-06 16:13:36 -0800

    add generated files for 64 bit

---
+++ build_windows_64/third_party/cares/cares/include/cmake_install.cmake
@@ -0,0 +1,44 @@
+# Install script for directory: C:/work/projects/grpc_alex/third_party/cares/cares/include
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/grpc")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Devel" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES
+    "C:/work/projects/grpc_alex/third_party/cares/cares/include/ares.h"
+    "C:/work/projects/grpc_alex/third_party/cares/cares/include/ares_version.h"
+    "C:/work/projects/grpc_alex/build_windows_64/third_party/cares/cares/ares_build.h"
+    "C:/work/projects/grpc_alex/third_party/cares/cares/include/ares_rules.h"
+    "C:/work/projects/grpc_alex/third_party/cares/cares/include/ares_dns.h"
+    )
+endif()
+
